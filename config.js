d189ad714fe89475d36b88b983415b28e6151476
tomohiro ikeda(池田 知洋) <tikeda@topcon.com>
2026-07-30 14:02:51 +0900

    feat: add static page structure, styles, and config

---
+++ config.js
@@ -0,0 +1,7 @@
+export const KEYGEN_ACCOUNT = "496af046-7b61-4484-84e8-cc48d558dbc7";
+export const KEYGEN_API = "https://api.keygen.sh";
+export const POLICY_NAME = "CoordyNavi_Promotion_1Year";
+export const PREFIX = "PPL";
+export const SEP = "_";
+export const PAD = 3;
+export const MAX_COUNT = 100;
